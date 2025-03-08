41b8fcc08d17a0f22475ce852cea0c6d1811136f
Tion Blackmon <tion.blackmon@outlook.com>
2025-03-08 08:57:57 -0600

    Added some comments explaining the use of type guard in the root page.tsx in /app,  so I can better remember.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ export default function Home() {
     <>
       <h1 className="mb-5">
         {/* Type Guard - TS concept */}
+        {/* In the context of Google Firebase, the `User` can be an object or false. TypeScript doesn't know if the `displayName` property can be used in either cases, so we need to do some type narrowing. */}
+        {/* Below is a type guard. This ensures user isn't false before we access the displayName property. */}
         Welcome, {user && typeof user !== 'boolean' ? user.displayName || 'Guest': 'Guest'}!
       </h1>
     </>
