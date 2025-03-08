1af76ffe5b3670ccc54372f61700262997d2430b
Tion Blackmon <tion.blackmon@outlook.com>
2025-03-08 10:56:03 -0600

    In ViewDirector.tsx, I had to import usePathname to force the /register page to load for unaauthenticated users. When using Link component from next/link, when a user clicks the Register button, the URL would change but the register page didn't render for some reason.

--- src/utils/context/ViewDirector.tsx
+++ src/utils/context/ViewDirector.tsx
@@ -3,6 +3,7 @@ import Loading from '@/Components/Loading';
 import SignInComponent from '@/Components/SignInComponent';
 import NavBar from '@/Components/NavBar';
 import { ReactNode } from 'react';
+import { usePathname } from 'next/navigation';
 
 interface ViewDirectorProps {
   children: ReactNode;
@@ -12,12 +13,22 @@ function ViewDirectorBasedOnUserAuthStatus({ children }: ViewDirectorProps) {
   console.log('ViewDirector is rendering')
 
   const { user, userLoading } = useAuth();
+  const pathname = usePathname();
+
   console.log("User state in ViewDirector: ", user);
   console.log("UserLoading state: ", userLoading);
 
   // if user state is null, then show loader
   if (userLoading) {
     return <Loading />;
+  }
+
+  // NOTE: This allows unauthenticated users to access the register page. For some reason, Next.js is having issues rendering the register page after I imported Link from next/link. To get around this, excluding the register page from authentication enforcement needed.
+  // Step 1 - import { usePathname } from 'next/navigation';
+  // Step 2 - const pathname = usePathname(); --- add this above the if statement that checks userLoading.
+  // Step 3 - Below this line, add the logic that should return the register component.
+  if (!user && pathname === "/register") {
+    return children;
   }
 
   // what the user should see if they are logged in
