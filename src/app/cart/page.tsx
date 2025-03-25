817d32c36efaede3e4e17a9e02cf56da5ac0d810
Tion Blackmon <tion.blackmon@outlook.com>
2025-03-24 19:55:58 -0500

    Now renderingcart subtotal based on the quantity of a cart item and it's price, plus any other items in the cart.

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -8,12 +8,24 @@ import Image from 'next/image';
 export default function CartPage() {
   const cartItems = useContext(CartContext);
   const removeFromCart = useRemoveFromCart();
+  let cartSubtotal = 0;
+
+  cartItems.forEach((item) => {
+    if (item.itemQuantity > 1) {
+      const itemPrice = item.product.price * item.itemQuantity;
+      cartSubtotal += itemPrice;
+    } else if (item.itemQuantity === 1) {
+      cartSubtotal += item.product.price;
+    }
+    return cartSubtotal;
+  });
 
   if (cartItems.length === 0) {
     return (
       <div className='d-flex flex-column align-items-center my-5'>
         <h1>Cart</h1>
         <h2 className="my-5">Uh oh! Your shopping cart is empty. Ready to add some items? 😊</h2>
+        <Button variant='outline-dark'>View products</Button>
       </div>
     )
   }
@@ -39,6 +51,11 @@ export default function CartPage() {
               <Button variant='link' onClick={() => removeFromCart(item.id)} className='mx-3'>Remove</Button>
             </div>
           ))}
+      </div>
+
+      <div className='d-flex'>
+        <h4>Subtotal: ${cartSubtotal}</h4>
+        <Button variant='outline-dark' className='mx-5'>Checkout</Button>
       </div>
     </div>
   );
