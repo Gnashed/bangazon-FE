4dc0376ceaaed78a3ea041aec888655809e5ac50
Tion Blackmon <tion.blackmon@outlook.com>
2025-03-26 10:22:41 -0500

    Added better alignment in Checkout Page

--- src/app/cart/checkout/page.tsx
+++ src/app/cart/checkout/page.tsx
@@ -1,29 +1,29 @@
 'use client';
 
-import Button from 'react-bootstrap/Button';
 import CheckoutForm from '@/Components/forms/checkout/CheckoutForm';
 
 // TODO: Might be better to use Bootstrap Rows and Columns
 export default function CheckoutPage() {
   return (
-    <>
+    <div>
       <h1 className='text-center my-5'>Checkout</h1>
+      
       <div className='d-flex justify-content-center my-5'>
+
+        {/* Left side */}
         <div className='d-flex flex-column align-items-center'>
-          {/* Left side */}
-          <div className='d-flex flex-column align-items-center'>
-            {/* TODO: Render shipping form. */}
-            <CheckoutForm />
-          </div>
-          
-          {/* Right side */}
-          <div>
-            {/* Cart Item Components */}
-            <h4>Shipping</h4>
-            <h4>Total</h4>
-          </div>
+          <CheckoutForm />
+        </div>
+        
+        {/* Right side */}
+        <div className='d-flex flex-column align-items-center m-5'>
+            {/* TODO: Render cart item components */}
+
+            <h4>Shipping: </h4>
+            <h4>Total: </h4>
+        
         </div>
       </div>
-    </>
+    </div>
   );
 }
